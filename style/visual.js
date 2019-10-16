63317265027f7c37a78f0e33d5ced201cadb76c5
Artem <53619450+TyT-NICK@users.noreply.github.com>
2019-10-16 09:58:09 +0800

    messenger selector

--- style/visual.js
+++ style/visual.js
@@ -1,3 +1,7 @@
+$(function () {
+    $('header').load('header.html');
+})
+
 $('.bots-nav-item').click(function (e) {
     $('.bots-nav-item').removeClass('selected');
     $(this).addClass('selected');
@@ -7,4 +11,24 @@ $(function () {
     if ($('.bots').children.length) {
         $('.bots').append("<p>нет ботов в данной категории</p>")
     }
+})
+
+$('#messenger-selector').click(function (e) {
+    if ($(e.target).hasClass('hideable'))
+        $(e.target).addClass('hidden');
+});
+
+$('.create-new-btn').click(function (e) {
+    e.preventDefault();
+    $('#messenger-selector').removeClass('hidden');
+})
+
+// Переделать чекинг
+$('#messenger-selector input[type=checkbox]').change(function() {
+    $('.go-to-create-btn').removeClass('disabled');
+})
+
+$('.btn').click(function (e) {
+    if ($(this).hasClass('disabled'))
+        e.preventDefault();
 })
